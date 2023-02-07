6b13c746dd45242d657004e89c024a30e437ffd4
AxmatxonovLutfullo <121826973+AxmatxonovLutfullo@users.noreply.github.com>
2023-02-07 16:39:19 +0500

    Delete PalindromeNumber.cpp

--- Two Sum/Palindrome Number/PalindromeNumber.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    bool isPalindrome(int x) {
-        string s1 = to_string(x);
-        string s2 = s1;
-        reverse(s2.begin(),s2.end()); 
-        return (s1 == s2);
-    }
-};
